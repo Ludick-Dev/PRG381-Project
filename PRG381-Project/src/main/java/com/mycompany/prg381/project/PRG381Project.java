b12e2fa0af33d404b530069e8fbd719246f32712
Ruan Ludick <ruludick2@gmail>
2023-08-26 07:56:38 +0200

    Added .java file

---
+++ PRG381-Project/src/main/java/com/mycompany/prg381/project/PRG381Project.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prg381.project;
+
+/**
+ *
+ * @author rulud
+ */
+public class PRG381Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
