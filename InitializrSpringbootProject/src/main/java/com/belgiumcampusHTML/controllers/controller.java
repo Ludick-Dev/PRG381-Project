b58755dbe10fd36a6f4117c931211d534c14f7a8
George Pieterse <111984830+Derik15@users.noreply.github.com>
2023-08-30 14:14:57 +0200

    Add files via upload
    This is the one I have been working on, trying to get the HTML files to work, if you guys wanted to take a look.

---
+++ InitializrSpringbootProject/src/main/java/com/belgiumcampusHTML/controllers/controller.java
@@ -0,0 +1,29 @@
+package com.belgiumcampusHTML.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {
+
+  @GetMapping("/")
+    public String showHomePage() {
+        return "index"; 
+    }
+
+    @GetMapping("/")
+    public String showRegistrationPage() {
+        return "register"; 
+    }
+
+    @GetMapping("/admin")
+    public String showLoginPage() {
+        return "admin";
+    }
+    
+        @GetMapping("/main")
+    public String showMainPage() {
+        return "main";
+    }
+
+}
